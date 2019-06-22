0600bd45a19cad3b03e43cffc32d20dbf2e03492
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-06-22 11:20:42 -0500

    added titles dropdown isntead of link

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,13 +1,51 @@
 import React from "react";
 
-//Sample NAVBAR UPDATED FROM BULMA WEBSITE
+//Sample NAVBAR UPDATED FROM BULMA WEBSITE, added Dropdown for titles
 const Navbar = props => (
   <nav class="level">
     <p class="level-item has-text-centered">
-      <a class="link is-info" href="/titles" alt="titles">Titles</a>
+      <div class="dropdown is-active">
+        <div class="dropdown-trigger">
+          <button
+            class="button"
+            aria-haspopup="true"
+            aria-controls="dropdown-menu"
+          >
+            <span>Dropdown button</span>
+            <span class="icon is-small">
+              <i class="fas fa-angle-down" aria-hidden="true" />
+            </span>
+          </button>
+        </div>
+        <div class="dropdown-menu" id="dropdown-menu" role="menu">
+          <div class="dropdown-content">
+            <a href="/" class="dropdown-item">
+                Title #1
+            </a>
+            <hr class="dropdown-divider" />
+            <a class="dropdown-item" href="/">
+                Title #2
+            </a>
+            <hr class="dropdown-divider" />
+            <a href="/" class="dropdown-item is-active">
+                Title #3
+            </a>
+            <hr class="dropdown-divider" />
+            <a href="/" class="dropdown-item">
+                Title #4
+            </a>
+            <hr class="dropdown-divider" />
+            <a href="/" class="dropdown-item">
+                Title #5
+            </a>
+          </div>
+        </div>
+      </div>
     </p>
     <p class="level-item has-text-centered">
-      <a class="link is-info" href="/" alt="home">Home</a>
+      <a class="link is-info" href="/" alt="home">
+        Home
+      </a>
     </p>
     <p class="level-item has-text-centered">
       <a class="link is-info" href="/" alt="finalproject">
@@ -25,7 +63,9 @@ const Navbar = props => (
       </figure>
     </p>
     <p class="level-item has-text-centered">
-      <a class="link is-info" href="/" alt="settings">Settings</a>
+      <a class="link is-info" href="/" alt="settings">
+        Settings
+      </a>
     </p>
   </nav>
 );
