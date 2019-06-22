a284417dffa012ddae732bdf05feda363f7c0659
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-06-22 11:29:10 -0500

    updated navbar component to reflect navbar classes for bulma

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,71 +2,65 @@ import React from "react";
 
 //Sample NAVBAR UPDATED FROM BULMA WEBSITE, added Dropdown for titles
 const Navbar = props => (
-  <nav class="level">
-    <p class="level-item has-text-centered">
-      <div class="dropdown is-active">
-        <div class="dropdown-trigger">
-          <button
-            class="button"
-            aria-haspopup="true"
-            aria-controls="dropdown-menu"
-          >
-            <span>Dropdown button</span>
-            <span class="icon is-small">
-              <i class="fas fa-angle-down" aria-hidden="true" />
-            </span>
-          </button>
-        </div>
-        <div class="dropdown-menu" id="dropdown-menu" role="menu">
-          <div class="dropdown-content">
-            <a href="/" class="dropdown-item">
-                Title #1
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+      </a>
+  
+      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+  
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item">
+          Home
+        </a>
+  
+        <a class="navbar-item">
+          Documentation
+        </a>
+  
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            More
+          </a>
+  
+          <div class="navbar-dropdown">
+            <a class="navbar-item">
+              About
             </a>
-            <hr class="dropdown-divider" />
-            <a class="dropdown-item" href="/">
-                Title #2
+            <a class="navbar-item">
+              Jobs
             </a>
-            <hr class="dropdown-divider" />
-            <a href="/" class="dropdown-item is-active">
-                Title #3
+            <a class="navbar-item">
+              Contact
             </a>
-            <hr class="dropdown-divider" />
-            <a href="/" class="dropdown-item">
-                Title #4
-            </a>
-            <hr class="dropdown-divider" />
-            <a href="/" class="dropdown-item">
-                Title #5
+            <hr class="navbar-divider" />
+            <a class="navbar-item">
+              Report an issue
             </a>
           </div>
         </div>
       </div>
-    </p>
-    <p class="level-item has-text-centered">
-      <a class="link is-info" href="/" alt="home">
-        Home
-      </a>
-    </p>
-    <p class="level-item has-text-centered">
-      <a class="link is-info" href="/" alt="finalproject">
-        <strong>Final Project</strong>
-      </a>
-    </p>
-    <p class="level-item has-text-centered">
-      <figure class="image is-128x128">
-        <img
-          class="is-rounded link is-info"
-          src="https://bulma.io/images/placeholders/128x128.png"
-          alt="profile"
-          href="/profile"
-        />
-      </figure>
-    </p>
-    <p class="level-item has-text-centered">
-      <a class="link is-info" href="/" alt="settings">
-        Settings
-      </a>
-    </p>
+  
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">
+              Log in
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </nav>
 );
 
