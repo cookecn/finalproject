c43855d0ff0ad6dbd4c29a789b537629cc0b6055
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-06-22 11:25:42 -0500

    added modal component

---
+++ client/src/components/Modal/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//ADDED sample Modal from Bulma, will need to add Javascript "onclick" function to make this activate
+const Modal = props => (
+<div class="modal is-active">
+  <div class="modal-background"></div>
+  <div class="modal-content">
+
+    feeditem
+
+  </div>
+  <button class="modal-close is-large" aria-label="close"></button>
+</div>
+);
+
+export default Modal;
