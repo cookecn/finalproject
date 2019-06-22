88d638eadff0916bdcadf53d39135e1536726ace
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-06-22 11:52:55 -0500

    added container component

---
+++ client/src/components/Container/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Container = props => (
+  <div class="container">
+    <div class="notification">
+      This container is <strong>centered</strong> on desktop.
+    </div>
+  </div>
+);
+
+export default Container;
