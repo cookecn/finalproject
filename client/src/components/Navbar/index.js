42950cbd22a37099b9a5e00d013e486f859c5e93
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-06-22 11:13:03 -0500

    updated navbar to reflect a circular profile image instead of text

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,26 +1,33 @@
 import React from "react";
 
-
 //Sample NAVBAR UPDATED FROM BULMA WEBSITE
 const Navbar = props => (
-        <nav class="level">
-            <p class="level-item has-text-centered">
-                <a class="link is-info">Titles</a>
-            </p>
-            <p class="level-item has-text-centered">
-                <a class="link is-info">Home</a>
-            </p>
-            <p class="level-item has-text-centered">
-                <a class="link is-info"><strong>Final Project</strong></a>
-            </p>
-            <p class="level-item has-text-centered">
-                <a class="link is-info">Profile</a>
-            </p>
-            <p class="level-item has-text-centered">
-                <a class="link is-info">Settings</a>
-            </p>
-        </nav>
+  <nav class="level">
+    <p class="level-item has-text-centered">
+      <a class="link is-info" href="/titles" alt="titles">Titles</a>
+    </p>
+    <p class="level-item has-text-centered">
+      <a class="link is-info" href="/" alt="home">Home</a>
+    </p>
+    <p class="level-item has-text-centered">
+      <a class="link is-info" href="/" alt="finalproject">
+        <strong>Final Project</strong>
+      </a>
+    </p>
+    <p class="level-item has-text-centered">
+      <figure class="image is-128x128">
+        <img
+          class="is-rounded link is-info"
+          src="https://bulma.io/images/placeholders/128x128.png"
+          alt="profile"
+          href="/profile"
+        />
+      </figure>
+    </p>
+    <p class="level-item has-text-centered">
+      <a class="link is-info" href="/" alt="settings">Settings</a>
+    </p>
+  </nav>
 );
 
 export default Navbar;
-
