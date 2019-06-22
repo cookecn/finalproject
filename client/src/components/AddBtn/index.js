bba447688fc383ddb1b2cd5ba9e0d7d321f9bcd6
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-06-22 11:50:29 -0500

    added submitbtn and deletebtn as components

---
+++ client/src/components/AddBtn/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+//ADDED AddBtn/SubmitBtn as a component
+const AddBtn = props => (
+    <a class="button is-primary is-focused">Focus</a>
+);
+
+export default AddBtn;
---
+++ client/src/components/DeleteBtn/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//ADDED DeleteBtn as a component
+const DeleteBtn = props => (
+    <a class="button is-danger is-focused">Focus</a>
+);
+
+
+export default DeleteBtn;
+
