c213d436e14cfa5fe1af0ce3f2c3a5da0c2f3c37
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-06-22 10:27:48 -0500

    added sample navbar component, for testing

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+//Sample NAVBAR UPDATED FROM BULMA WEBSITE
+const Navbar = props => (
+        <nav class="level">
+            <p class="level-item has-text-centered">
+                <a class="link is-info">Titles</a>
+            </p>
+            <p class="level-item has-text-centered">
+                <a class="link is-info">Home</a>
+            </p>
+            <p class="level-item has-text-centered">
+                <a class="link is-info"><strong>Final Project</strong></a>
+            </p>
+            <p class="level-item has-text-centered">
+                <a class="link is-info">Profile</a>
+            </p>
+            <p class="level-item has-text-centered">
+                <a class="link is-info">Settings</a>
+            </p>
+        </nav>
+);
+
+export default Navbar;
+
