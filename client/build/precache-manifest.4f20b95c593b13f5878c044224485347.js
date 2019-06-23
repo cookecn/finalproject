46216f3b5ef23f9085d7c50d20cd9186245239bf
Colby Cooke <colby.n.cooke@vanderbilt.edu>
2019-06-22 22:02:39 -0500

    push to heroku

---
+++ client/build/precache-manifest.4f20b95c593b13f5878c044224485347.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f8e900367f12a456fe58f6f5ad4670",
+    "url": "/cookecn/finalproject/index.html"
+  },
+  {
+    "revision": "011d330a61895528d89d",
+    "url": "/cookecn/finalproject/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/cookecn/finalproject/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "011d330a61895528d89d",
+    "url": "/cookecn/finalproject/static/js/main.f2da7a3b.chunk.js"
+  },
+  {
+    "revision": "a9eb7c64efbd94f49c69",
+    "url": "/cookecn/finalproject/static/js/runtime~main.e4f2e238.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cookecn/finalproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
